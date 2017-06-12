be972b99388ea87245d7f35d59e35eaa3d606090
Siyu Kat Wang <siyukw@gmail.com>
2017-06-12 11:04:13 -0700

    can't find path

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python
 
-""" 
-    This is the code to accompany the Lesson 1 (Naive Bayes) mini-project. 
+"""
+    This is the code to accompany the Lesson 1 (Naive Bayes) mini-project.
 
     Use a Naive Bayes Classifier to identify emails by their authors
-    
+
     authors and labels:
     Sara has label 0
     Chris has label 1
 """
-    
+
 import sys
 from time import time
 sys.path.append("../tools/")
@@ -22,12 +22,15 @@ from email_preprocess import preprocess
 features_train, features_test, labels_train, labels_test = preprocess()
 
 
-
+t0 = time()
 
 #########################################################
 ### your code goes here ###
+from sklearn import naive_bayes
 
-
+clf = GaussianNB()
+clf.fit(features_train, labels_train)
+pred = clf.predict(features_test)
+print accuracy_score(pred, labels_test)
+print "training time:", round(time()-t0, 3), "s"
 #########################################################
-
-
